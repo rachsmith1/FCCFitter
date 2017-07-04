ae94a5f43fee4000a7eddf594396bde97296f498
clementhelsens <clement.helsens@cern.ch>
2017-07-04 09:59:36 +0200

    add eos setup

--- setupFCC.sh
+++ setupFCC.sh
@@ -1,3 +1,3 @@
-# Setup ROOT and gcc
-# added back by Michele
 source /cvmfs/fcc.cern.ch/sw/0.8.1/init_fcc_stack.sh
+export EOS_MGM_URL="root://eospublic.cern.ch"
+source /afs/cern.ch/project/eos/installation/client/etc/setup.sh
