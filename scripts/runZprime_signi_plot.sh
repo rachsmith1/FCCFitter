13f8d00b22fc207e52caef6c3728b0e65368fd23
clementhelsens <clement.helsens@cern.ch>
2017-07-17 12:24:34 +0200

    add scripts to run significance and significance plots

---
+++ scripts/runZprime_signi_plot.sh
@@ -0,0 +1,3 @@
+ python python/significance_plot.py -f "Zprime_ee_fcc_v01.json Zprime_ee_cms.json" -n "fcc cms" -p ee
+ python python/significance_plot.py -f "Zprime_mumu_fcc_v01.json Zprime_mumu_cms.json" -n "fcc cms" -p mumu
+ python python/significance_plot.py -f "Zprime_ll_fcc_v01.json Zprime_ll_cms.json" -n "fcc cms" -p ll
